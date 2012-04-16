6606ea125dffc6c2dff042c5ffc867510f554f02
Albert Taruc <albert.taruc@gmail.com>
2012-04-15 18:11:06 -0700

    First full commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCounter::Application.config.secret_token = '1a7bdbe46d2da3a25280741ada6c71cd2fe743f64189092ea96ee6f16b4b7f3ae514638d7a48e9e5b5fbb615f447d63b28e7237c6ea11609e696d8e9c3987a53'
